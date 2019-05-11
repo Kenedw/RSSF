088c47e7cdd7c12b70e8de4e97433049d7a1f103
Alexandre <alexandrerodrigueslopesfilho@gmail.com>
2019-05-11 16:25:42 -0300

    modelo classes

---
+++ host.py
@@ -0,0 +1,11 @@
+
+import pacote.py
+
+class host():
+	def __init__(self, ID, ID_host, pacote):
+		self.ID = ID #ID do host
+		self.ID_host = ID_host
+		self.pacote = pacote.pacote()
+
+	#... 
+		
---
+++ pacote.py
@@ -0,0 +1,8 @@
+
+class pacote():
+	def __init__(self, origem, destino, size)
+		self.size = size
+		self.origem = origem
+		self.destino = destino
+
+		#...
