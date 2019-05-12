1132ca559cd14e183ea541b6ec4cac4ddd16a949
KenedW <kenedwanderson@live.com>
2019-05-11 23:27:46 -0300

    fix host energy

--- carga.py
+++ carga.py
@@ -12,5 +12,5 @@ def CreatNetwork(argv):
     station = router(i["id"],10)
     HostsLocatios = utills.gerate_xy(len(i["hosts"]),10,10)
     for index,j in enumerate(i["hosts"]):
-      station.insertHost(host(i["id"],j["id"],HostsLocatios[index],))
+      station.insertHost(host(i["id"],j["id"],HostsLocatios[index],j["energy"]))
       print(json.dumps(j))
--- host.py
+++ host.py
@@ -3,14 +3,14 @@
 from redes import redes
 class host(redes):
 	# ID = 0
-	def __init__(self, ID_router,ID, xy):
+	def __init__(self, ID_router,ID, position,energy):
 		super().__init__()
 		# super().__init__(link)
 		self.ID = ID #ID do host
 		self.ID_router = ID_router
-		self.energia = energia
+		self.energy = energy
 		# self.packet = packet.packet()
-		self.coordinates = xy #tuple
+		self.coordinates = position #tuple
 		print("host "+ID+" criado")
 
 
