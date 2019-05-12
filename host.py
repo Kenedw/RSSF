68bdb31779f087c384309a09defba7465f581ba6
KenedW <kenedwanderson@live.com>
2019-05-11 22:48:11 -0300

    fix host

--- host.py
+++ host.py
@@ -1,11 +1,13 @@
 
-import packet
-
-class host():
-	def __init__(self, ID, ID_host, packet):
+# import packet
+import redes
+class host(redes.redes):
+	# ID = 0
+	def __init__(self, ID_router,ID, xy,link):
+		super().__init__(link)
 		self.ID = ID #ID do host
-		self.ID_host = ID_host
-		self.packet = packet.packet()
+		self.ID_router = ID_router
+		# self.packet = packet.packet()
+		self.coordinates = xy #tuple
 
-	#... 
-		
+
