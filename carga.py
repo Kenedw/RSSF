f76b184dd023e930ff3184389ecd39bb3672bc83
Alexandre Lopes <alexandrerodrigueslopesfilho@gmail.com>
2019-05-12 19:45:06 -0300

    plot v.1.1 está contido no carga.py

--- carga.py
+++ carga.py
@@ -3,6 +3,11 @@ import sys
 import utills
 from router import router
 from host import host
+import matplotlib.pyplot as plt
+import math
+
+altura = 50
+largura =50
 
 
 def CreatNetwork(argv):
@@ -14,4 +19,32 @@ def CreatNetwork(argv):
     for index,j in enumerate(i["hosts"]): #roda em cada host
       station.insertHost(host(i["id"],j["id"],HostsLocatios[index],j["energy"],j["range"]))
     routerList.append(station)
+#INICIO PLOT
+  plt.axis([0, largura, 0, altura]) #dimensao x e y varia de 0 a 20	
+  plt.xlabel("Metros")
+  plt.ylabel("Metros")
+  
+  print(HostsLocatios)
+  print(station)
+
+  ID=1
+  for aux in HostsLocatios:
+    x,y = aux[0], aux[1]
+    plt.plot( x, y, 'ro') 					 #(x,y, circlered)
+    plt.annotate(ID,(x,y))					 #nome aos pontos, coloca H na frente do ID
+    ID +=1
+    area = math.pi * (10**2)				 #pegar o range de outra forma
+    plt.scatter(x, y, s=area*250, alpha=0.1) #opaco, desenha a area sobre o host
+
+  #for aux2 in station:    	 # pecorre o vetor com todos os nos, e liga as retas
+   # z,w = aux2[0], aux2[1]
+
+    #dz = route[i+1].position[0] - route[i].position[0]
+    #dw = route[i+1].position[1] - route[i].position[1]
+
+   # plt.arrow(z, w,)
+
+  plt.show()
+#FIM PLOT
   return routerList
+
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
+import matplotlib.pyplot as plt
+import math
 import json
 import carga
-# import plot
 import run
-import router
+import utills
+from router import router
+from host import host
+
+altura = 20
+largura =20
 
 if __name__ == "__main__":
   routers = carga.CreatNetwork(sys.argv[1]) #carrega os dados da rede
-  run.analise(routers,sys.argv[1]) 
-
-
-  # plot.map(router.hostlist, routers ,10 ,10 )
-
- 
+  run.analise(routers,sys.argv[1])   
