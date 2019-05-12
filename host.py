50ae3f3ba52877cb91ba929fe12f4c68a7de4922
Alexandre Lopes <alexandrerodrigueslopesfilho@gmail.com>
2019-05-12 00:12:56 -0300

    alcance

--- host.py
+++ host.py
@@ -7,7 +7,14 @@ class host(redes):
 	def __init__(self, ID_router,ID, position,energy,reach):
 		super().__init__()
 		# super().__init__(link)
+
 		# self.packet = packet.packet()
+
+
+	def __init__(self, ID_router,ID, xy,link, alcance):
+		super().__init__(link)
+
+
 		self.ID = ID #ID do host
 		self.ID_router = ID_router
 		self.energy = energy
@@ -15,7 +22,13 @@ class host(redes):
 		print("host "+ID+" criado")
 		self.reach = reach 
 
+
 	# def Hellou(self, topologia):
 	#  return self
+
+		self.coordinates = xy #tuple
+		self.alcance = alcance #gerar range aleatoriamente
+
+
 
 
--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import utills
 import host 
+import router
 
 def plot(host.coordinates, 10, 10):
 
@@ -10,12 +11,13 @@ def plot(host.coordinates, 10, 10):
 	plt.xlabel("Metros")
 	plt.ylabel("Metros")
 
-	for host in host_list: 
+	for host in router.hostlist: 
 		plt.plot(host.coordinates[0], host.coordinates[1], 'ro') #(x,y, circlered)
 
-		area = math.pi *(host.range)
+		area = math.pi * (host.alcance**2)
+		plt.scatter(host.coordinates[0], host.coordinates[1], s=area*250, alpha=1) #opaco
 
-	 	
+  for i in range(len())
 
 
 
